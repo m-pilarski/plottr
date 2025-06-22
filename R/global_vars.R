3205298d3b086577c7147245cb97a22d7f568fbd
m-pilarski <89189235+m-pilarski@users.noreply.github.com>
2025-06-22 13:43:54 +0200

    update

---
+++ R/global_vars.R
@@ -0,0 +1,4 @@
+globalVariables(c(
+  "l", "t", "margin_bottom", "margin_left", "margin_right", "margin_top",
+  "name"
+))
---
+++ R/panel_pos_rel.R
@@ -0,0 +1,53 @@
+#' Title
+#'
+#' @param .x ...
+#'
+#' @returns integer()
+#'
+#' @examples NULL
+unit_to_mm <- function(.x){
+  grid::convertUnit(.x, "mm", valueOnly=TRUE)
+}
+
+#' Title
+#'
+#' @param .ggplot ...
+#'
+#' @returns tibble()
+#' @export
+#'
+#' @examples NULL
+calc_element_margin_data <- function(.ggplot){
+
+  .plot_grob <- ggplot2::ggplotGrob(.ggplot)
+
+  .element_pos_rel <-
+    .plot_grob |>
+    purrr::chuck("layout") |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      name = name,
+      margin_top = sum(vctrs::vec_slice(
+        unit_to_mm(purrr::chuck(.plot_grob, "heights")), seq_len(t - 1)
+      )),
+      margin_left = sum(vctrs::vec_slice(
+        unit_to_mm(purrr::chuck(.plot_grob, "widths")), seq_len(l - 1)
+      )),
+      margin_bottom = sum(vctrs::vec_slice(
+        unit_to_mm(purrr::chuck(.plot_grob, "heights")), -seq_len(t)
+      )),
+      margin_right = sum(vctrs::vec_slice(
+        unit_to_mm(purrr::chuck(.plot_grob, "widths")), -seq_len(l)
+      )),
+      .keep="none"
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::mutate(
+      dplyr::across(-name, \(..x){ggplot2::unit(..x, "mm")}),
+      margin_y = margin_top + margin_bottom,
+      margin_x = margin_left + margin_right,
+    )
+
+  return(.panel_pos_rel)
+
+}
